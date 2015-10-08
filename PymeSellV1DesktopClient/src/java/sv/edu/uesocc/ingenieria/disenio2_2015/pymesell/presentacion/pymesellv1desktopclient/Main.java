e307acf0d4d6cc1cbac49e9f45ce3c5c360d6604
girondave <girondave@gmail.com>
2015-10-08 14:30:48 -0600

    add project to repository
    ...

---
+++ PymeSellV1DesktopClient/src/java/sv/edu/uesocc/ingenieria/disenio2_2015/pymesell/presentacion/pymesellv1desktopclient/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.disenio2_2015.pymesell.presentacion.pymesellv1desktopclient;
+
+/**
+ *
+ * @author David
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola a todos!!!");
+    }
+    
+}
